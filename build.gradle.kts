4054f533e4546b66f6dfa43ee4c16906a3dd34a2
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-05-03 10:39:04 +0100

    added aspect, outbound, inbound, service, model, repository packages.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,51 @@
 plugins {
     id("java")
+    id("pmd")
+    id("jacoco")
+    id("checkstyle")
+    id("idea")
+    id("eclipse")
+    id("java-library")
+    id("org.springframework.boot") version "3.1.9"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("org.liquibase.gradle") version "2.0.3"
+    id("com.github.spotbugs") version "5.2.1"
 }
 
-group = "com.github.tkaczenko"
+val springCloudVersion by extra("2022.0.4")
+val lombokVersion by extra("1.18.22")
+val checkstyleVersion by extra("10.12.5")
+val sevntuChecksVersion by extra("1.44.1")
+
+group = "com.github.tkaczenko.auditor"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+    }
+}
+
 dependencies {
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+    implementation("org.apache.commons:commons-lang3")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    checkstyle("com.puppycrawl.tools:checkstyle:${checkstyleVersion}")
+    checkstyle("com.github.sevntu-checkstyle:sevntu-checks:${sevntuChecksVersion}")
 }
 
 tasks.test {
