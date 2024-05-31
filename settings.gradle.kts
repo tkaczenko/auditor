5b355c10d0b6cb3cf973a32b676dfb2ae70bbbf0
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-05-31 17:22:21 +0100

    Refactored with Gradle modules, specifically core, inbound and outbound.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,8 @@
-rootProject.name = "auditor"
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
 
+rootProject.name = "auditor"
+include("core")
+include("inbound")
+include("outbound")
