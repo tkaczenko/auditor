b32d10db88131c979386efaa2dabf96c72056626
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-06-07 12:03:28 +0100

    Moved common configurations into auditor.library-conventions.gradle.kts and auditor.application-conventions.gradle.kts.

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -8,17 +8,4 @@ dependencies {
     testImplementation(libs.h2)
     testImplementation(libs.wiremock)
     testImplementation(libs.rest.assured)
-
-    implementation(project(":core"))
-    implementation(project(":inbound"))
-    implementation(project(":outbound"))
-    implementation(project(":outbound-feign"))
-    implementation(project(":cleanup"))
-
-    implementation(platform(libs.spring.boot))
-    implementation(platform(libs.spring.cloud))
-
-    implementation(libs.spring.boot.starter.web)
-    implementation(libs.spring.boot.starter.data.jpa)
-    implementation(libs.spring.cloud.starter.openfeign)
 }
