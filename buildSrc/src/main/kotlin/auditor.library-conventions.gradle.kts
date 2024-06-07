1ecda531d74ddc8d10095ec438e612692c0be99a
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-06-07 12:03:28 +0100

    Moved common configurations into auditor.library-conventions.gradle.kts and auditor.application-conventions.gradle.kts.

--- buildSrc/src/main/kotlin/auditor.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/auditor.library-conventions.gradle.kts
@@ -1,4 +1,13 @@
+import org.gradle.accessors.dm.LibrariesForLibs
+
+val libs = project.extensions.getByName("libs") as LibrariesForLibs
+
 plugins {
     `java-library`
     id("auditor.java-conventions")
+}
+
+dependencies {
+    implementation(platform(libs.spring.boot))
+    implementation(platform(libs.spring.cloud))
 }
