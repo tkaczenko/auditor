da5d1edee4f1a132202a049324edca6de6b7a840
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-06-07 12:03:28 +0100

    Moved common configurations into auditor.library-conventions.gradle.kts and auditor.application-conventions.gradle.kts.

--- buildSrc/src/main/kotlin/auditor.application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/auditor.application-conventions.gradle.kts
@@ -1,4 +1,24 @@
+import gradle.kotlin.dsl.accessors._b57f3335cbc6d1ccbac183e6469782cf.implementation
+import org.gradle.accessors.dm.LibrariesForLibs
+
+val libs = project.extensions.getByName("libs") as LibrariesForLibs
+
 plugins {
     application
     id("auditor.java-conventions")
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":inbound"))
+    implementation(project(":outbound"))
+    implementation(project(":outbound-feign"))
+    implementation(project(":cleanup"))
+
+    implementation(platform(libs.spring.boot))
+    implementation(platform(libs.spring.cloud))
+
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.data.jpa)
+    implementation(libs.spring.cloud.starter.openfeign)
 }
