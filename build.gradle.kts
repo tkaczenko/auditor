81919d267cee9e47e278b970863d3d1441d167bb
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-06-12 10:38:34 +0100

    Added aggregateJavadoc.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,3 +29,21 @@ tasks.check {
     dependsOn(tasks.named<TestReport>("testAggregateTestReport"))
     dependsOn(tasks.named<JacocoReport>("testCodeCoverageReport"))
 }
+
+val exportedProjects = listOf(
+    ":core", ":inbound", ":outbound", ":outbound-feign", ":cleanup", ":starter"
+)
+
+tasks.register("aggregateJavadoc", Javadoc::class) {
+    group = "documentation"
+    description = "Generates Javadoc for all exported subprojects"
+
+    val javaSources =
+        exportedProjects.flatMap { project(it).sourceSets["main"].allJava.srcDirs }
+    val classpathFiles =
+        exportedProjects.flatMap { project(it).sourceSets["main"].compileClasspath }
+
+    source = files(javaSources).asFileTree
+    classpath = files(classpathFiles)
+    setDestinationDir(layout.buildDirectory.dir("/docs/javadoc").get().asFile)
+}
