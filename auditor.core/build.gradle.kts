5707491e1f3546331977a90e4d9bd4c0dafa4109
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-06-16 22:14:44 +0100

    Added descriptions, groups, and versions.

--- auditor.core/build.gradle.kts
+++ auditor.core/build.gradle.kts
@@ -2,6 +2,11 @@ plugins {
     id("auditor.library-conventions")
 }
 
+description = """
+Provides persistence of audit data, including payloads, headers, and metadata, in an SQL database. 
+It defines the core entities and repositories for storing and retrieving audit records.
+""".trimIndent()
+
 dependencies {
     implementation(libs.spring.boot.starter.data.jpa)
     implementation(libs.spring.boot.starter.web)
--- auditor.inbound/build.gradle.kts
+++ auditor.inbound/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("auditor.library-conventions")
 }
 
+description = """
+Provides auditing of inbound HTTP requests and responses to the application's controllers. 
+It leverages Spring's request interceptor mechanism to capture incoming requests and responses, 
+and persists the audit data.
+""".trimIndent()
+
 dependencies {
     testImplementation(libs.spring.test)
 
