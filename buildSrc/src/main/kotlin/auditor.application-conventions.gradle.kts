49df49f6af3afc299c1a6f6e51e018fd6a64c569
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-06-26 21:44:31 +0100

    Moved dependencies of demo into buildSrc. Updated AuditRecord and AuditRequestResponse.

--- buildSrc/src/main/kotlin/auditor.application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/auditor.application-conventions.gradle.kts
@@ -21,4 +21,11 @@ dependencies {
     implementation(libs.micrometer.brave)
 
     implementation(libs.h2)
+
+    testImplementation(libs.spring.boot.starter.test)
+    testImplementation(libs.java.diff)
+    testImplementation(libs.wiremock)
+    testImplementation(libs.rest.assured)
+    testImplementation(libs.assertj)
+    testImplementation(libs.awaitility)
 }
