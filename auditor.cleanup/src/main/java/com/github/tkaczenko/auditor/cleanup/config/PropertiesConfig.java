2765150ba8f4631d8eedea2a78df2882610213d2
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-07-17 09:29:11 +0100

    Renamed Configuration to Config.

--- auditor.cleanup/src/main/java/com/github/tkaczenko/auditor/cleanup/config/PropertiesConfig.java
+++ auditor.cleanup/src/main/java/com/github/tkaczenko/auditor/cleanup/config/PropertiesConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.Configuration;
  * application, including scheduling-related properties.
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class PropertiesConfiguration {
+public class PropertiesConfig {
 
   /** Provides configuration properties for the auditor scheduling functionality. */
   @Data
