9dcb56247a04d924dcd399434fdbb498aed78355
Andrii Tkachenko <andrii.tkaczenko@gmail.com>
2024-09-01 21:01:34 +0100

    Updated build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,11 @@ repositories {
 }
 
 dependencies {
+    testReportAggregation(project(":auditor.core"))
+    testReportAggregation(project(":auditor.inbound"))
     testReportAggregation(project(":auditor.cleanup"))
+    jacocoAggregation(project(":auditor.core"))
+    jacocoAggregation(project(":auditor.inbound"))
     jacocoAggregation(project(":auditor.cleanup"))
 }
 
